5135df918e1093539366babe6f7841ae5acdc963
Scott <smasherprog@gmail.com>
2017-01-07 14:36:42 -0800

    Added IOS partial

--- src/ios/CGFrameProcessor.cpp
+++ src/ios/CGFrameProcessor.cpp
@@ -7,7 +7,7 @@ namespace SL {
         struct CGFrameProcessorImpl {
 
 			std::shared_ptr<THREAD_DATA> Data;
-			std::unique_ptr<char[]> ImageBuffer;
+					std::unique_ptr<char[]> OldImageBuffer, NewImageBuffer;
 			size_t ImageBufferSize;
 		};
 
@@ -26,7 +26,11 @@ namespace SL {
             auto ret = DUPL_RETURN::DUPL_RETURN_SUCCESS;
             _CGFrameProcessorImpl->Data = data;
             _CGFrameProcessorImpl->ImageBufferSize  = Height(*data->SelectedMonitor)*Width(*data->SelectedMonitor)*PixelStride;
-            _CGFrameProcessorImpl->ImageBuffer = std::make_unique<char[]>(_CGFrameProcessorImpl->ImageBufferSize);
+          	if (_CGFrameProcessorImpl->Data->CaptureDifMonitor) {//only need the old buffer if difs are needed. If no dif is needed, then the image is always new
+				_CGFrameProcessorImpl->OldImageBuffer = std::make_unique<char[]>(_CGFrameProcessorImpl->ImageBufferSize);
+			}
+			_CGFrameProcessorImpl->NewImageBuffer = std::make_unique<char[]>(_CGFrameProcessorImpl->ImageBufferSize);
+		
             
             
 			return ret;
@@ -42,7 +46,7 @@ namespace SL {
             auto width = CGImageGetWidth(imageRef);
             auto height = CGImageGetHeight(imageRef);
             auto colorSpace = CGColorSpaceCreateDeviceRGB();
-            auto rawData = _CGFrameProcessorImpl->ImageBuffer.get();
+            auto rawData = _CGFrameProcessorImpl->ImageBufferSize.get();
             auto bytesPerPixel = 4;
             auto bytesPerRow = bytesPerPixel * width;
             auto bitsPerComponent = 8;
@@ -60,9 +64,26 @@ namespace SL {
                 imgrect.left =  imgrect.top=0;
                 imgrect.right =width;
                 imgrect.bottom = height;
-                auto img = CreateImage(imgrect, PixelStride, 0,_CGFrameProcessorImpl->ImageBuffer.get());
+                auto img = CreateImage(imgrect, PixelStride, 0,_CGFrameProcessorImpl->ImageBufferSize.get());
                 _CGFrameProcessorImpl->Data->CaptureEntireMonitor(*img, *_CGFrameProcessorImpl->Data->SelectedMonitor);
             }
+			if (_CGFrameProcessorImpl->Data->CaptureDifMonitor) {
+					//user wants difs, lets do it!
+					auto newimg = CreateImage(ret, PixelStride, 0, _CGFrameProcessorImpl->NewImageBuffer.get());
+					auto oldimg = CreateImage(ret, PixelStride, 0, _CGFrameProcessorImpl->OldImageBuffer.get());
+					auto imgdifs = GetDifs(*oldimg, *newimg);
+
+					for (auto& r : imgdifs) {
+						auto padding = (r.left *PixelStride) + ((Width(*newimg) - r.right)*PixelStride);
+						auto startsrc = _CGFrameProcessorImpl->NewImageBuffer.get();
+						startsrc += (r.left *PixelStride) + (r.top *PixelStride *Width(*newimg));
+
+						auto difimg = CreateImage(r, PixelStride, padding, startsrc);
+						_CGFrameProcessorImpl->Data->CaptureDifMonitor(*difimg, *_CGFrameProcessorImpl->Data->SelectedMonitor);
+
+					}
+					std::swap(_CGFrameProcessorImpl->NewImageBuffer, _CGFrameProcessorImpl->OldImageBuffer);
+				}
 			return Ret;
 		}
 

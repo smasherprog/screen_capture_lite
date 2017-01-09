533eb6d6d1ea5ee4dce5ec7377947c8c18790661
Scott <smasherprog@gmail.com>
2017-01-08 21:35:25 -0800

    Fixed apple build

--- src/ios/ThreadRunner.cpp
+++ src/ios/ThreadRunner.cpp
@@ -8,7 +8,7 @@ namespace SL{
 			void RunCaptureMouse(std::shared_ptr<Mouse_Thread_Data> data) {
 
 			}
-        void RunCapture(std::shared_ptr<THREAD_DATA> data){
+        void RunCapture(std::shared_ptr<Monitor_Thread_Data> data){
             
             
             TryCapture<CGFrameProcessor>(data);

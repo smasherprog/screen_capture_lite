a8fb617ed3c96cf56ce7b5d6c34317a17f330a26
scott lee <smasherprog@gmail.com>
2017-04-19 20:27:16 -0700

    Fix for Mac Screen Scaling

--- src/ios/GetMonitors.cpp
+++ src/ios/GetMonitors.cpp
@@ -19,10 +19,11 @@ namespace SL{
                 //only include non-mirrored displays
                 if(CGDisplayMirrorsDisplay(displays[i]) == kCGNullDirectDisplay){
                     
-    
-                    auto width = CGDisplayPixelsWide(displays[i]);
-                    auto height = CGDisplayPixelsHigh(displays[i]);
-      
+                    auto dismode =CGDisplayCopyDisplayMode(displays[i]);
+                    
+                    auto width = CGDisplayModeGetPixelWidth(dismode);
+                    auto height = CGDisplayModeGetPixelHeight(dismode);
+                    CGDisplayModeRelease(dismode);
                     auto r = CGDisplayBounds(displays[i]);
                  
                     auto name = std::string("Monitor ") + std::to_string(displays[i]);

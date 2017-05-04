78cbfb1345205024f42cea2da58d6c53b0fcac8e
Scott <smasherprog@gmail.com>
2017-05-03 18:43:48 -0700

    Converted example to spaces

--- Example/Screen_Capture_Example.cpp
+++ Example/Screen_Capture_Example.cpp
@@ -17,70 +17,70 @@
 
 int main()
 {
-	std::cout << "Starting Capture Demo" << std::endl;
-	std::atomic<int> realcounter;
-	realcounter = 0;
-	SL::Screen_Capture::ScreenCaptureManager framgrabber;
-	auto start = std::chrono::high_resolution_clock::now();
+    std::cout << "Starting Capture Demo" << std::endl;
+    std::atomic<int> realcounter;
+    realcounter = 0;
+    SL::Screen_Capture::ScreenCaptureManager framgrabber;
+    auto start = std::chrono::high_resolution_clock::now();
 
-	framgrabber.onFrameChanged([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
-		std::cout << "height  " << Height(img) << "  width  " << Width(img) << std::endl;
-		auto r = realcounter.fetch_add(1);
-		auto s = std::to_string(r) + std::string(" D") + std::string(".jpg");
-		auto size = RowStride(img)*Height(img);
+    framgrabber.onFrameChanged([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
+        std::cout << "height  " << Height(img) << "  width  " << Width(img) << std::endl;
+        auto r = realcounter.fetch_add(1);
+        auto s = std::to_string(r) + std::string(" D") + std::string(".jpg");
+        auto size = RowStride(img)*Height(img);
 
-		auto imgbuffer(std::make_unique<char[]>(size));
-		if (Height(img) > 2000) {
-			auto s = std::chrono::high_resolution_clock::now();
-			std::cout << "TimeFromLast " << std::chrono::duration_cast<std::chrono::milliseconds>(s - start).count() << std::endl;
-			start = s;
-		}
-		Extract(img, imgbuffer.get(), size);
+        auto imgbuffer(std::make_unique<char[]>(size));
+        if (Height(img) > 2000) {
+            auto s = std::chrono::high_resolution_clock::now();
+            std::cout << "TimeFromLast " << std::chrono::duration_cast<std::chrono::milliseconds>(s - start).count() << std::endl;
+            start = s;
+        }
+        Extract(img, imgbuffer.get(), size);
 
-		//tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
-	});
+        //tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
+    });
 
-	framgrabber.onNewFrame([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
+    framgrabber.onNewFrame([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
 
-		auto r = realcounter.fetch_add(1);
-		auto s = std::to_string(r) + std::string(" E") + std::string(".jpg");
+        auto r = realcounter.fetch_add(1);
+        auto s = std::to_string(r) + std::string(" E") + std::string(".jpg");
 
-		auto size = RowStride(img)*Height(img);
-		auto imgbuffer(std::make_unique<char[]>(size));
-		Extract(img, imgbuffer.get(), size);
+        auto size = RowStride(img)*Height(img);
+        auto imgbuffer(std::make_unique<char[]>(size));
+        Extract(img, imgbuffer.get(), size);
 
-		//tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
+        //tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
 
-	});
-	framgrabber.onMouseChanged([&](const SL::Screen_Capture::Image* img, int x, int y) {
+    });
+    framgrabber.onMouseChanged([&](const SL::Screen_Capture::Image* img, int x, int y) {
 
-		auto r = realcounter.fetch_add(1);
-		auto s = std::to_string(r) + std::string(" M") + std::string(".png");
-		if (img) {
-			//std::cout << "New Mouse Image  x= " << x << " y= " << y << std::endl;
-			//lodepng::encode(s, (const unsigned char*)StartSrc(*img), Width(*img), Height(*img));
-		}
-		else {
-			//new coords 
-			//std::cout << "x= " << x << " y= " << y << std::endl;
-		}
+        auto r = realcounter.fetch_add(1);
+        auto s = std::to_string(r) + std::string(" M") + std::string(".png");
+        if (img) {
+            //std::cout << "New Mouse Image  x= " << x << " y= " << y << std::endl;
+            //lodepng::encode(s, (const unsigned char*)StartSrc(*img), Width(*img), Height(*img));
+        }
+        else {
+            //new coords 
+            //std::cout << "x= " << x << " y= " << y << std::endl;
+        }
 
-	});
+    });
 
-	framgrabber.setMonitorsToCapture([] {
-		return SL::Screen_Capture::GetMonitors();//specify which monitors you are interested in
-	});
+    framgrabber.setMonitorsToCapture([] {
+        return SL::Screen_Capture::GetMonitors();//specify which monitors you are interested in
+    });
 
-	framgrabber.setFrameChangeInterval(100);//100 ms
-	framgrabber.setMouseChangeInterval(100);//100 ms
+    framgrabber.setFrameChangeInterval(100);//100 ms
+    framgrabber.setMouseChangeInterval(100);//100 ms
 
-	framgrabber.Start();
+    framgrabber.Start();
 
-	while (true) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	}
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 
-	framgrabber.Stop();
-	return 0;
+    framgrabber.Stop();
+    return 0;
 }
 

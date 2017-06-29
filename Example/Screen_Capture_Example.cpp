cbc23205b7ba462fc8d1a4265a1e2071dad2131b
scott <smasherprog@gmail.com>
2017-06-28 18:13:34 -0700

    updated linux build-Performance

--- Example/Screen_Capture_Example.cpp
+++ Example/Screen_Capture_Example.cpp
@@ -1,13 +1,12 @@
 #include "ScreenCapture.h"
-#include <iostream> 
-#include <chrono> 
+#include <iostream>
+#include <chrono>
 #include <atomic>
 #include <thread>
 #include <string>
 #include <vector>
 
-
-//THESE LIBRARIES ARE HERE FOR CONVINIENCE!! They are SLOW and ONLY USED FOR HOW THE LIBRARY WORKS!
+// THESE LIBRARIES ARE HERE FOR CONVINIENCE!! They are SLOW and ONLY USED FOR HOW THE LIBRARY WORKS!
 #define TJE_IMPLEMENTATION
 #include "tiny_jpeg.h"
 #define LODEPNG_COMPILE_PNG
@@ -24,57 +23,59 @@ int main()
     onNewFramecounter = 0;
     auto onNewFramestart = std::chrono::high_resolution_clock::now();
 
-    auto framgrabber = SL::Screen_Capture::CreateScreeCapture([]() {
-        return SL::Screen_Capture::GetMonitors();
-    }).onFrameChanged([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
-        std::cout << "height  " << Height(img) << "  width  " << Width(img) << std::endl;
-        auto r = realcounter.fetch_add(1);
-        auto s = std::to_string(r) + std::string(" D") + std::string(".jpg");
-        auto size = RowStride(img)*Height(img);
+    auto framgrabber =
+        SL::Screen_Capture::CreateScreeCapture([]() { return SL::Screen_Capture::GetMonitors(); })
+            .onFrameChanged([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
+                std::cout << "height  " << Height(img) << "  width  " << Width(img) << std::endl;
+                auto r = realcounter.fetch_add(1);
+                auto s = std::to_string(r) + std::string(" D") + std::string(".jpg");
+                auto size = RowStride(img) * Height(img);
 
-        auto imgbuffer(std::make_unique<char[]>(size));
-        //you can extract the image as is in BGRA, or Convert
-        //Extract(img, imgbuffer.get(), size);
-        ExtractAndConvertToRGBA(img, imgbuffer.get(), size);
+                auto imgbuffer(std::make_unique<char[]>(size));
+                // you can extract the image as is in BGRA, or Convert
+                // Extract(img, imgbuffer.get(), size);
+                ExtractAndConvertToRGBA(img, imgbuffer.get(), size);
 
-        //tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
-    }).onNewFrame([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
+                // tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
+            })
+            .onNewFrame([&](const SL::Screen_Capture::Image& img, const SL::Screen_Capture::Monitor& monitor) {
 
-        auto r = realcounter.fetch_add(1);
-        auto s = std::to_string(r) + std::string(" E") + std::string(".jpg");
+                auto r = realcounter.fetch_add(1);
+                auto s = std::to_string(r) + std::string(" E") + std::string(".jpg");
 
-        auto size = RowStride(img)*Height(img);
-        auto imgbuffer(std::make_unique<char[]>(size));
-        //Extract(img, imgbuffer.get(), size);
+                auto size = RowStride(img) * Height(img);
+                auto imgbuffer(std::make_unique<char[]>(size));
+                // Extract(img, imgbuffer.get(), size);
 
-        ExtractAndConvertToRGBA(img, imgbuffer.get(), size);
+                ExtractAndConvertToRGBA(img, imgbuffer.get(), size);
 
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - onNewFramestart).count() >= 1000) {
-            std::cout << "onNewFrame fps" << onNewFramecounter << std::endl;
-            onNewFramecounter = 0;
-            onNewFramestart = std::chrono::high_resolution_clock::now();
-        }
-        onNewFramecounter += 1;
-            tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
-    }).onMouseChanged([&](const SL::Screen_Capture::Image* img, int x, int y) {
+                if(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() -
+                                                                         onNewFramestart).count() >= 1000) {
+                    std::cout << "onNewFrame fps" << onNewFramecounter << std::endl;
+                    onNewFramecounter = 0;
+                    onNewFramestart = std::chrono::high_resolution_clock::now();
+                }
+                onNewFramecounter += 1;
+                tje_encode_to_file(s.c_str(), Width(img), Height(img), 4, (const unsigned char*)imgbuffer.get());
+            })
+            .onMouseChanged([&](const SL::Screen_Capture::Image* img, int x, int y) {
 
-        auto r = realcounter.fetch_add(1);
-        auto s = std::to_string(r) + std::string(" M") + std::string(".png");
-        if (img) {
-            //std::cout << "New Mouse Image  x= " << x << " y= " << y << std::endl;
-            //lodepng::encode(s, (const unsigned char*)StartSrc(*img), Width(*img), Height(*img));
-        }
-        else {
-            //new coords 
-            //std::cout << "x= " << x << " y= " << y << std::endl;
-        }
+                auto r = realcounter.fetch_add(1);
+                auto s = std::to_string(r) + std::string(" M") + std::string(".png");
+                if(img) {
+                    // std::cout << "New Mouse Image  x= " << x << " y= " << y << std::endl;
+                    // lodepng::encode(s, (const unsigned char*)StartSrc(*img), Width(*img), Height(*img));
+                } else {
+                    // new coords
+                    // std::cout << "x= " << x << " y= " << y << std::endl;
+                }
 
-    }).start_capturing();
+            })
+            .start_capturing();
 
-    framgrabber.setFrameChangeInterval(std::chrono::milliseconds(100));//100 ms
-    framgrabber.setMouseChangeInterval(std::chrono::milliseconds(100));//100 ms
+    framgrabber.setFrameChangeInterval(std::chrono::milliseconds(100)); // 100 ms
+    framgrabber.setMouseChangeInterval(std::chrono::milliseconds(100)); // 100 ms
 
     std::this_thread::sleep_for(std::chrono::seconds(10));
     return 0;
 }
-
--- src/linux/X11FrameProcessor.cpp
+++ src/linux/X11FrameProcessor.cpp
@@ -3,87 +3,99 @@
 #include <assert.h>
 #include <vector>
 
+namespace SL
+{
+namespace Screen_Capture
+{
+    X11FrameProcessor::X11FrameProcessor()
+    {
+    }
 
-namespace SL {
-	namespace Screen_Capture {
-		X11FrameProcessor::X11FrameProcessor()
-		{
+    X11FrameProcessor::~X11FrameProcessor()
+    {
 
-		}
+        if(ShmInfo) {
+            shmdt(ShmInfo->shmaddr);
+            shmctl(ShmInfo->shmid, IPC_RMID, 0);
+            XShmDetach(SelectedDisplay, ShmInfo.get());
+        }
+        if(Image) {
+            XDestroyImage(Image);
+        }
+        if(SelectedDisplay) {
+            XCloseDisplay(SelectedDisplay);
+        }
+    }
+    DUPL_RETURN X11FrameProcessor::Init(std::shared_ptr<Thread_Data> data, Monitor& monitor)
+    {
+        auto ret = DUPL_RETURN::DUPL_RETURN_SUCCESS;
+        Data = data;
+        SelectedMonitor = monitor;
+        SelectedDisplay = XOpenDisplay(NULL);
+        if(!SelectedDisplay) {
+            return DUPL_RETURN::DUPL_RETURN_ERROR_EXPECTED;
+        }
 
-		X11FrameProcessor::~X11FrameProcessor()
-		{
+        RootWindow = XRootWindow(SelectedDisplay, Index(SelectedMonitor));
+        if(!RootWindow) {
+            return DUPL_RETURN::DUPL_RETURN_ERROR_EXPECTED;
+        }
+        auto visual = DefaultVisual(SelectedDisplay, Index(SelectedMonitor));
+        auto depth = DefaultDepth(SelectedDisplay, Index(SelectedMonitor));
 
-			if (ShmInfo) {
-				shmdt(ShmInfo->shmaddr);
-				shmctl(ShmInfo->shmid, IPC_RMID, 0);
-				XShmDetach(SelectedDisplay, ShmInfo.get());
-			}
-			if (Image) {
-				XDestroyImage(Image);
-			}
-			if (SelectedDisplay) {
-				XCloseDisplay(SelectedDisplay);
-			}
-		}
-		DUPL_RETURN X11FrameProcessor::Init(std::shared_ptr<Thread_Data> data, Monitor& monitor) {
-			auto ret = DUPL_RETURN::DUPL_RETURN_SUCCESS;
-			Data = data;
-            SelectedMonitor = monitor;
-			SelectedDisplay = XOpenDisplay(NULL);
-			if (!SelectedDisplay) {
-				return DUPL_RETURN::DUPL_RETURN_ERROR_EXPECTED;
-			}
+        ShmInfo = std::make_unique<XShmSegmentInfo>();
 
+        Image = XShmCreateImage(SelectedDisplay,
+                                visual,
+                                depth,
+                                ZPixmap,
+                                NULL,
+                                ShmInfo.get(),
+                                Width(SelectedMonitor),
+                                Height(SelectedMonitor));
+        ShmInfo->shmid = shmget(IPC_PRIVATE, Image->bytes_per_line * Image->height, IPC_CREAT | 0777);
 
-			RootWindow = XRootWindow(SelectedDisplay, Index(SelectedMonitor));
-			if (!RootWindow) {
-				return DUPL_RETURN::DUPL_RETURN_ERROR_EXPECTED;
-			}
-			auto visual = DefaultVisual(SelectedDisplay, Index(SelectedMonitor));
-			auto depth = DefaultDepth(SelectedDisplay, Index(SelectedMonitor));
+        ShmInfo->readOnly = False;
+        ShmInfo->shmaddr = Image->data = (char*)shmat(ShmInfo->shmid, 0, 0);
 
-			ShmInfo = std::make_unique<XShmSegmentInfo>();
+        XShmAttach(SelectedDisplay, ShmInfo.get());
 
-			Image = XShmCreateImage(SelectedDisplay, visual, depth, ZPixmap, NULL, ShmInfo.get(), Width(SelectedMonitor), Height(SelectedMonitor));
-			ShmInfo->shmid = shmget(IPC_PRIVATE, Image->bytes_per_line * Image->height, IPC_CREAT | 0777);
+        return ret;
+    }
+    //
+    // Process a given frame and its metadata
+    //
+    DUPL_RETURN X11FrameProcessor::ProcessFrame()
+    {
+        auto Ret = DUPL_RETURN_SUCCESS;
+        ImageRect ret;
+        ret.left = ret.top = 0;
+        ret.right = Width(SelectedMonitor);
+        ret.bottom = Height(SelectedMonitor);
 
-			ShmInfo->readOnly = False;
-			ShmInfo->shmaddr = Image->data = (char*)shmat(ShmInfo->shmid, 0, 0);
+        // check to see if the display has changed
+        auto t = XOpenDisplay(NULL);
+        auto monh = DisplayHeight(t, Id(SelectedMonitor));
+        auto monw = DisplayWidth(t, Id(SelectedMonitor));
+        XCloseDisplay(t);
 
-			XShmAttach(SelectedDisplay, ShmInfo.get());
+        if(monh != ret.bottom || monw != ret.right) {
+            return DUPL_RETURN_ERROR_EXPECTED;
+        }
 
+        if(!XShmGetImage(SelectedDisplay, RootWindow, Image, 0, 0, AllPlanes)) {
+            return DUPL_RETURN_ERROR_EXPECTED;
+        }
 
-			return ret;
-		}
-		//
-		// Process a given frame and its metadata
-		//
-		DUPL_RETURN X11FrameProcessor::ProcessFrame()
-		{
-			auto Ret = DUPL_RETURN_SUCCESS;
-			ImageRect ret;
-			ret.left = ret.top = 0;
-			ret.right = Width(SelectedMonitor);
-			ret.bottom = Height(SelectedMonitor);
-            
-            //check to see if the display has changed
-            auto t = XOpenDisplay(NULL);
-            auto monh = DisplayHeight(t, Id(SelectedMonitor));
-            auto monw = DisplayWidth(t, Id(SelectedMonitor));
-            XCloseDisplay(t);
-            
-            if( monh!= ret.bottom || monw!= ret.right){
-                   return DUPL_RETURN_ERROR_EXPECTED;  
-            }
+        if(Data->CaptureEntireMonitor && !Data->CaptureDifMonitor) {
 
-			if(!XShmGetImage(SelectedDisplay, RootWindow, Image, 0, 0, AllPlanes)){
-                return DUPL_RETURN_ERROR_EXPECTED;
-            }
-			memcpy(NewImageBuffer.get(), Image->data, PixelStride*ret.right*ret.bottom);
+            auto wholeimg = Create(ret, PixelStride, 0, Image->data);
+            Data->CaptureEntireMonitor(wholeimg, SelectedMonitor);
+        } else {
+            memcpy(NewImageBuffer.get(), Image->data, PixelStride * ret.right * ret.bottom);
             ProcessMonitorCapture(*Data, *this, SelectedMonitor, ret);
-			return Ret;
-		}
-
-	}
+        }
+        return Ret;
+    }
+}
 }

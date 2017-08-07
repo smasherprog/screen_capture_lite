09c2f7f512375ad5ee9d70257b239c3ed6d5cc97
Scott <smasherprog@gmail.com>
2017-08-06 20:25:58 -0700

    window mouse cleanup

--- src/windows/GDIMouseProcessor.cpp
+++ src/windows/GDIMouseProcessor.cpp
@@ -108,18 +108,18 @@ namespace SL {
                 RECT windowRect;
                 GetWindowRect(SelectedWindow, &windowRect);
                 POINT cursorPos = { cursorInfo.ptScreenPos.x , cursorInfo.ptScreenPos.y };
-      
+                auto mouseloc = Point{ lastx - windowRect.left, lasty - windowRect.top };
                 //if the mouse is over the window continue!
                 if (PtInRect(&windowRect, cursorPos) == TRUE) {
                     if (memcmp(NewImageBuffer.get(), OldImageBuffer.get(), bi.biSizeImage) != 0) {
-                        Data->WindowCaptureData.OnMouseChanged(&wholeimg, Point{ lastx - windowRect.left, lasty - windowRect.top}, *wnd);
+                        Data->WindowCaptureData.OnMouseChanged(&wholeimg, mouseloc, *wnd);
                         std::swap(NewImageBuffer, OldImageBuffer);
                     } if (Last_x != lastx || Last_y != lasty) {
-                        Data->WindowCaptureData.OnMouseChanged(nullptr, Point{ lastx - windowRect.left,  lasty - windowRect.top }, *wnd);
+                        Data->WindowCaptureData.OnMouseChanged(nullptr, mouseloc, *wnd);
                     }
                 }
-                else  if (memcmp(NewImageBuffer.get(), OldImageBuffer.get(), bi.biSizeImage) != 0) {
-                    Data->WindowCaptureData.OnMouseChanged(&wholeimg, Point{ lastx - windowRect.left,  lasty - windowRect.top }, *wnd);
+                else if (memcmp(NewImageBuffer.get(), OldImageBuffer.get(), bi.biSizeImage) != 0) {
+                    Data->WindowCaptureData.OnMouseChanged(&wholeimg, mouseloc, *wnd);
                     std::swap(NewImageBuffer, OldImageBuffer);
                 }
             }

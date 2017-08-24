f0fc264f223add2708cbfa18631ab6c694cb8144
scott lee <smasherprog@gmail.com>
2017-08-23 21:12:17 -0700

    Updated mac support for new api

--- src/ios/GetWindows.cpp
+++ src/ios/GetWindows.cpp
@@ -11,12 +11,8 @@ namespace SL
 namespace Screen_Capture
 {
 
-    std::vector<Window> GetWindows(const std::string& name1, WindowStringMatch searchby)
+    std::vector<Window> GetWindows()
     {
-        auto nametofind = name1;
-        std::transform(nametofind.begin(), nametofind.end(), nametofind.begin(), [](char c) {
-            return std::tolower(c, std::locale());
-        }); // convert to lower
         auto windowList = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
         std::vector<Window> ret;
         CFIndex numWindows = CFArrayGetCount(windowList );
@@ -28,22 +24,8 @@ namespace Screen_Capture
             auto cfwindowname = static_cast<CFStringRef>(CFDictionaryGetValue(dict, kCGWindowName));
             CFStringGetCString(cfwindowname, buffer, 400, kCFStringEncodingUTF8);
             std::string windowname=buffer;
-            std::transform(windowname.begin(), windowname.end(), windowname.begin(), [](char c) {
-                return std::tolower(c, std::locale());
-            }); // convert to lower
-            
-            bool found = false;
-            if(searchby == WindowStringMatch::STARTSWITH) {
-                auto f = windowname.substr(0, nametofind.size());
-                found = f == nametofind;
-            } else if(searchby == WindowStringMatch::EXACT) {
-                found = windowname == nametofind;
-            } else if(searchby == WindowStringMatch::CONTAINS) {
-                found = windowname.find(nametofind) != std::string::npos;
-            }
-            std::cout<<windowname<<std::endl;
-            
-            if(found){
+        
+           
                Window w;
                 CFNumberGetValue(static_cast<CFNumberRef>(CFDictionaryGetValue(dict, kCGWindowNumber)), kCFNumberIntType, &windowid);
                 w.Handle = static_cast<size_t>(windowid);
@@ -58,8 +40,7 @@ namespace Screen_Capture
                 w.Size.y = static_cast<int>(rect.size.height);
                 
                 memcpy(w.Name, windowname.c_str(), windowname.size() + 1);
-                ret.push_back(w); 
-            }
+                ret.push_back(w);
         }
         CFRelease(windowList);
         return ret;

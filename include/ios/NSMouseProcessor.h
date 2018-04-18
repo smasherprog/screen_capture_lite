6f45d723f98d875f894d4375c2fde318f96e6c18
scott <smasherprog@gmail.com>
2018-04-17 20:14:50 -0700

    fix typo

--- include/ios/NSMouseProcessor.h
+++ include/ios/NSMouseProcessor.h
@@ -10,7 +10,7 @@ namespace SL {
         class NSMouseProcessor: public BaseFrameProcessor {
             int Last_x =0;
             int Last_y =0;
-            std::unique_ptr<unsigned char[]> OldImageBuffer
+            std::unique_ptr<unsigned char[]> OldImageBuffer;
         public:
             const int MaxCursurorSize=32;
             DUPL_RETURN Init(std::shared_ptr<Thread_Data> data);

0ab3ff90b784138ea197084747b15f8aa8c2973f
scott <smasherprog@gmail.com>
2018-04-22 09:35:49 -0700

    linux travis tests

--- src/linux/GetMonitors.cpp
+++ src/linux/GetMonitors.cpp
@@ -11,13 +11,15 @@ namespace Screen_Capture
     std::vector<Monitor> GetMonitors()
     {
         std::vector<Monitor> ret;
-
+        printf("got here");
         Display* display = XOpenDisplay(NULL);
+        printf("got here1");
         if(display == NULL){
             return ret;
         }
         int nmonitors = 0;
         XineramaScreenInfo* screen = XineramaQueryScreens(display, &nmonitors);
+         printf("got here2");
         if(screen == NULL){
             XCloseDisplay(display);
             return ret;
@@ -29,9 +31,10 @@ namespace Screen_Capture
             auto name = std::string("Display ") + std::to_string(i);
             ret.push_back(CreateMonitor(
                 i, screen[i].screen_number, screen[i].height, screen[i].width, screen[i].x_org, screen[i].y_org, name, 1.0f));
-        }
-        XFree(screen); 
+        }   printf("got here3");
+        XFree(screen);    printf("got here4");
         XCloseDisplay(display);
+           printf("got here5");
         return ret;
     }
 }

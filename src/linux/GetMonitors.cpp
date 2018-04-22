36237f5015c914787d64f41f9809bc922eb06692
scott <smasherprog@gmail.com>
2018-04-22 10:39:13 -0700

    fixing linux

--- src/linux/GetMonitors.cpp
+++ src/linux/GetMonitors.cpp
@@ -11,22 +11,28 @@ namespace Screen_Capture
 
     std::vector<Monitor> GetMonitors()
     {
-        std::vector<Monitor> ret; 
-        Display* display = XOpenDisplay(NULL); 
-        if(display == NULL){
+      std::vector<Monitor> ret;
+
+        Display* display = XOpenDisplay(NULL);
+        if(display==NULL){
             return ret;
         }
-        auto nmonitors = ScreenCount(display);
+        int nmonitors = 0;
+        XineramaScreenInfo* screen = XineramaQueryScreens(display, &nmonitors);
+         if(screen==NULL){ 
+            XCloseDisplay(display);
+            return ret;
+        }
         ret.reserve(nmonitors);
-        
+       
         for(auto i = 0; i < nmonitors; i++) {
-            auto screen = ScreenOfDisplay(display, i);
-        
+
             auto name = std::string("Display ") + std::to_string(i);
             ret.push_back(CreateMonitor(
-                i, i, screen->height, screen->width, 0, 0, name, 1.0f));
-        }     
-        XCloseDisplay(display); 
+                i, screen[i].screen_number, screen[i].height, screen[i].width, screen[i].x_org, screen[i].y_org, name, 1.0f));
+        }
+        XFree(screen);
+        XCloseDisplay(display);
         return ret;
     }
 }

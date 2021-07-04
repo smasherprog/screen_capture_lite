820fdccb92f60542b09d3b9342ca206f32d18352
scott lee <smasherprog@gmail.com>
2021-07-04 14:19:59 -0700

    Updated ignores

--- build-unity-library.sh
+++
@@ -1,1 +0,0 @@
-make && g++ -dynamiclib -undefined suppress -flat_namespace ./CMakeFiles/screen_capture_lite.dir/src/*.o -flat_namespace ./CMakeFiles/screen_capture_lite.dir/src/ios/*.o -o libscreen_capture_lite.dylib && cp libscreen_capture_lite.dylib './Example_UP/Assets/Plugins/ScreenCaptureLite/libscreen_capture_lite.dylib'
